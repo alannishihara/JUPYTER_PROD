334d931e286112293745e3a2b323cb28ca9cd9f3
Alan Nishihara <131835263+alannishihara@users.noreply.github.com>
2023-09-13 13:01:31 -0700

    Delete AN_FOR_LOOP.py

--- AN_FOR_LOOP.py
+++
@@ -1,9 +0,0 @@
-# for letter in 'HELLO!':
-#     print(letter)
-
-while True:
-    name = input('\nPlease enter your name: ')
-    if name == 'Tom':
-        print('\n\tAccess GRANTED, ' + name + '!')
-    else:
-        print('\n\tAccess DENIED, ' + name + '!')
